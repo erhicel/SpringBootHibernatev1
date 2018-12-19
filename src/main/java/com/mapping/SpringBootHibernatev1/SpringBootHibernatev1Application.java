d1fa8cb1e1818ec1bb1f36dc26fc8c84677cf2f6
erhicel <erhicel@gmail.com>
2018-12-19 10:02:16 +0100

    Otra forma de configurar CORS

--- src/main/java/com/mapping/SpringBootHibernatev1/SpringBootHibernatev1Application.java
+++ src/main/java/com/mapping/SpringBootHibernatev1/SpringBootHibernatev1Application.java
@@ -13,4 +13,21 @@ public class SpringBootHibernatev1Application {
 		SpringApplication.run(SpringBootHibernatev1Application.class, args);
 	}
 
+	// Otra forma de configurar CORS
+	// @SpringBootApplication
+	// public class DemoApplication {
+	// public static void main(String[] args) {
+	// SpringApplication.run(DemoApplication.class, args);
+	// }
+	// @Bean
+	// public WebMvcConfigurer corsConfigurer() {
+	// return new WebMvcConfigurerAdapter() {
+	// @Override
+	// public void addCorsMappings(CorsRegistry registry) {
+	// registry.addMapping("/products").allowedOrigins("http://localhost:8080");
+	// }
+	// };
+	// }
+	// }
+
 }
